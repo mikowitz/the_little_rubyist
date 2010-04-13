de2bfc3fdd1fe89926637a7e52fc28df548e1c34
Michael Berkowitz <michael.berkowitz@gmail.com>
2010-04-13 16:29:36 -0400

    Adds Rakefile and spec file.

---
+++ spec/helper.rb
@@ -0,0 +1,10 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'spec'
+require 'spec/autorun'
+
+require 'the_little_rubyist'
+
+Spec::Runner.configure do |config|
+
+end
